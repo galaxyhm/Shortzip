a662d3d465ec6fc323a533e29b4326cf62de0109
dowon <ehdnjs8745@gmail.com>
2023-04-27 14:13:41 +0900

    test\

--- django_app/ARTICLE_SUMMARY/accounts/models.py
+++ django_app/ARTICLE_SUMMARY/accounts/models.py
@@ -1,11 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    phone_number = models.IntegerField()
-    
-    
-    
+class User(AbstractUser):
+    age = models.IntegerField()
